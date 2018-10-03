cc2c2c4dc8a4f8806251829e76f7e04cc43dd4cb
Gabriel Cesar <gabriel.cesar555@gmail.com>
2018-10-03 20:09:56 -0300

    Finalização do processo

--- 2018.1-IAC/AB2.1-TP/src/main.c
+++ 2018.1-IAC/AB2.1-TP/src/main.c
@@ -1,33 +1,61 @@
-#include <unistd.h> 
-#include <sys/types.h> 
-#include <sys/wait.h> 
-#include <stdio.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (int argc, char *argv[], char *envp[]) {
+ 	int pid; //process identifier
+    int i;
+    char usage_cpu[100]; //command of cpu usage
+    char usage_mem[100]; //command of memory usage
+    char kill[100]; //command used to kill the process
 
-int pid ; /* identificador de processo */
+	pid = fork(); //process replication
+ 	if (pid < 0) { //if fork doesn't work
+		perror ("Erro: Fork failed.");
+		exit (-1); //terminates the process with error code -1
+	}
 
-pid = fork () ; /* replicacção do processo */
+	else if(pid > 0){ //if I'm the father process
+        //this 3 sprintf functions format the used commands
+        sprintf(usage_cpu, "%s%d%s", "ps -p ", pid, " -o pcpu | sed 1d | tr -d ' '");  //script to result CPU
+        sprintf(usage_mem, "%s%d%s", "pmap -x ", pid, " | grep total | awk '{print $4}'");  //script to result RAM
+        sprintf(kill, "%s%d", "kill -9 ", pid);
 
-if ( pid < 0 ) { /* se o fork não funcionou */
-	perror ("Erro: ") ;
-	exit (-1) ; /* encerra o processo com código de erro -1 */ 
-}
-else if( pid > 0 ) /* se sou o processo pai*/ 
-{
-	//TODO guarde a cada segundo o consumo de memória (em Kilobytes) e CPU (em porcentagem) do processo filho
-	//TODO após 10 segundos de execução, mate o proceso filho
-}
-else /* senão, sou o processo filho (pid == 0) */ 
-{
-	//TODO se argv[1] for igual a 'cpu', executar código com utilização intensa da UCP
-	//TODO se argv[1] for igual a 'cpu-mem', executar código com utilização intensa da UCP e da memória
+        for(i = 0; i <= 10; i++) {
+        	printf("Time: %ds\n", i);
+            printf("CPU usage(%%)\n");
+            
+            system(usage_cpu); //running the cpu usage command
+            printf("MEM usage(kbps)\n");
+            
+            system(usage_mem); //running the memory usage command
+            printf("------\n");
+            
+            sleep(1); //waits one second
+        }
 
-}
-perror ("Erro: ") ; /* execve nãoo funcionou */
+        system(kill); //running the killing command
+	}
 
-printf ("Tchau !\n") ;
-exit(0) ; /* encerra o processo com sucesso (código 0) */ 
+	else { //else I'm the son process
+		if(strcmp(argv[1], "ucp") == 0) {
+		    for( ; ; ){
+		    	usleep(100);
+		    }
+		}
+ 
+        if(strcmp(argv[1], "ucp-mem") == 0) {
+            for ( ; ; ){
+                malloc(sizeof(100000));
+                usleep(100);
+            }
+		}
+	}
 
-}
+	perror ("Erro") ; //execve didn't work
+ 	printf ("Finished!\n") ;
+	exit(0) ; //terminates the process with success (code 0) */
+ }
